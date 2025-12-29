25f336a3e3e423314061ef9bbb6525c60d800322
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2025-12-29 15:30:00 -0300

    users db connection admin

--- config/database.php
+++ config/database.php
@@ -62,6 +62,25 @@ return [
                 (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+        'admin' => [
+            'driver' => 'mysql',
+            'url' => env('DB_URL'),
+            'host' => env('DB_HOST_ADM', '127.0.0.1'),
+            'port' => env('DB_PORT_ADM', '3306'),
+            'database' => env('DB_DATABASE_ADM', 'laravel'),
+            'username' => env('DB_USERNAME_ADM', 'root'),
+            'password' => env('DB_PASSWORD_ADM', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
 
         'mariadb' => [
             'driver' => 'mariadb',
