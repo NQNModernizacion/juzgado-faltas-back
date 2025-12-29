459125942fd9163a7d2ebf4c7cbf139ddf5f46b9
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2025-12-29 15:36:47 -0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,22 +6,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    // use HasFactory, Notifiable;
+    use HasApiTokens, HasRoles;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $connection = 'admin';
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+    // /**
+    //  * The attributes that are mass assignable.
+    //  *
+    //  * @var list<string>
+    //  */
+    // protected $fillable = [
+    //     'password',
+    // ];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -30,19 +34,21 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+    protected string $guard_name = 'sanctum';
+
+
+    // /**
+    //  * Get the attributes that should be cast.
+    //  *
+    //  * @return array<string, string>
+    //  */
+    // protected function casts(): array
+    // {
+    //     return [
+    //         'email_verified_at' => 'datetime',
+    //         'password' => 'hashed',
+    //     ];
+    // }
 }
