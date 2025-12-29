56da30ffef562e460a02bbb5ddd5746b6050cfe6
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2025-12-29 17:17:24 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+
+/* Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:sanctum'); */
+
+// Route::prefix('auth')->group(function () {
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('refresh', [AuthController::class, 'refresh']); // a demanda
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('me', [AuthController::class, 'me']);
+});
+// });
