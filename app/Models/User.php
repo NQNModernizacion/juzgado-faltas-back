6ba8d45616a17ded745992f82dd30fa4d3e4292b
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2026-01-05 12:14:54 -0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Utils\ReadOnlyModel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -13,7 +15,7 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     // use HasFactory, Notifiable;
-    use HasApiTokens, HasRoles;
+    use HasApiTokens, HasRoles, ReadOnlyModel;
 
     protected $connection = 'admin';
     protected $table = 'users';
