829b9d33b7ff398a884f6fc4eaaaa6c8c627adff
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2026-04-23 12:03:52 -0300

    Create 2026_04_21_155030_create_causas_table.php

---
+++ database/migrations/2026_04_21_155030_create_causas_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('causas', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('grupo_acta_id')
+                ->constrained('grupos_actas')
+                ->restrictOnDelete();
+                // ->unique();
+
+            $table->string('numero_juzgado')->nullable();
+            $table->year('year')->nullable();
+            $table->string('numero_causa')->nullable()->unique();
+            $table->dateTime('fecha_alta_causa')->nullable();
+
+            $table->unsignedBigInteger('causa_id_padre')->nullable();
+            $table->dateTime('fecha_vinculacion_padre')->nullable();
+
+
+
+            // otros campos de la causa
+            $table->foreignId('estado_causa_id')->constrained('estados_generales', 'id')->nullable()->onDelete('set null');
+            $table->text('observacion')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('causas');
+    }
+};
