efe5842e84f27a87ab48672f5fcbb2913fa2bb99
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2026-04-23 12:05:20 -0300

    Create InfractorController.php

---
+++ app/Http/Controllers/InfractorController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Infractor;
+use App\Http\Requests\StoreInfractorRequest;
+use App\Http\Requests\UpdateInfractorRequest;
+
+class InfractorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreInfractorRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Infractor $infractor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateInfractorRequest $request, Infractor $infractor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Infractor $infractor)
+    {
+        //
+    }
+}
