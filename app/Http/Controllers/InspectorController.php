1354b600dbc4002c00e84750b24c2f60315dbd6b
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2026-04-23 12:05:22 -0300

    Create InspectorController.php

---
+++ app/Http/Controllers/InspectorController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Inspector;
+use Illuminate\Http\Request;
+
+class InspectorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Inspector $inspector)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Inspector $inspector)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Inspector $inspector)
+    {
+        //
+    }
+}
