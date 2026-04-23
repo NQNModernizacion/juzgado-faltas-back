8efc38c0aa7939eb108a4a5928e312fddb29d648
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2026-04-23 12:05:12 -0300

    Create CalleController.php

---
+++ app/Http/Controllers/CalleController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Calle;
+use Illuminate\Http\Request;
+
+class CalleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Calle $calle)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Calle $calle)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Calle $calle)
+    {
+        //
+    }
+}
