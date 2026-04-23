a44c0bd11a87f402674be4595e5e6fd6150721eb
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2026-04-23 12:03:42 -0300

    Create 2026_04_10_172623_create_actas_table.php

---
+++ database/migrations/2026_04_10_172623_create_actas_table.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('actas', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('grupo_acta_id')->nullable()
+                ->constrained('grupos_actas')
+                ->restrictOnDelete();
+                
+            $table->string('numero_acta', 50);
+            $table->string('year', 10)->nullable()->index('year_acta'); 
+            $table->foreignId('oficina_id')->nullable()->constrained('oficinas', 'id')->onDelete('set null');
+            $table->dateTime('fecha_labrada')->nullable();
+            $table->dateTime('fecha_carga')->nullable();
+            $table->foreignId('tipo_id')->nullable()->constrained('estados_generales', 'id')->onDelete('set null');
+            $table->foreignId('sub_tipo_id')->nullable()->constrained('estados_generales', 'id')->onDelete('set null');
+            $table->foreignId('ley_id')->nullable()->constrained('estados_generales', 'id')->onDelete('set null');
+
+            $table->string('lugar', 255)->nullable();
+
+            $table->foreignId('calle_id')->nullable()->constrained('calles', 'id')->onDelete('set null');
+            $table->unsignedInteger('numero_calle')->nullable();
+            $table->foreignId('cruce_id')->nullable()->constrained('calles', 'id')->onDelete('set null');
+
+            $table->foreignId('estado_acta_id')->nullable()->constrained('estados_generales', 'id')->onDelete('set null');
+            $table->dateTime('fecha_estado')->nullable();
+
+            $table->boolean('desestimada')->nullable();
+            // $table->foreignId('motivo_desestimado_id')->nullable()->constrained('estados_generales', 'id', "motivo_desestimado")->onDelete('set null');
+
+            // $table->foreignId('grado_acta_id')->nullable()->constrained('estados_generales', 'id', "grado_acta")->onDelete('set null');
+            $table->dateTime('fecha_notificado')->nullable();
+
+            $table->foreignId('inspector_1_id')->nullable()->constrained('inspectores', 'id')->onDelete('set null');
+            $table->foreignId('inspector_2_id')->nullable()->constrained('inspectores', 'id')->onDelete('set null');
+
+            $table->longText('infracciones')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('actas');
+    }
+};
