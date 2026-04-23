979adfeb3bbe3a7e5eaf0564c3ebf6dbd01ce6b2
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2026-04-23 12:03:59 -0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,11 @@ class DatabaseSeeder extends Seeder
         $this->call([
 
             RolesSeeder::class,
+            EstadosGeneralesSeeder::class,
+            OficinaSeeder::class,
+            InspectorSeeder::class,
+            CalleSeeder::class,
+
 
         ]);
         // User::factory(10)->create();
