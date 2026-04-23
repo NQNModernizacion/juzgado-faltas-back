6646b2ede3c8a94d6331824d685a21172479733a
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2026-04-23 12:04:18 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ActaController;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DatosActaController;
 use App\Http\Controllers\PDFController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,10 +24,10 @@ Route::middleware('auth:sanctum')->group(function () {
 
 Route::middleware('auth:sanctum')->prefix('admin')->group(function () {
     Route::get('bootstrap', [AdminController::class, 'index']);
-   Route::get('get_person_info/{dni}', [AdminController::class, 'getPersonInfoByDni']);
+    Route::get('get_person_info/{dni}', [AdminController::class, 'getPersonInfoByDni']);
     Route::post('users/{user}/sync-roles', [AdminController::class, 'syncRoles']);
     Route::post('roles/{role}/sync-permissions', [AdminController::class, 'syncRolePermissions']);
-    
+
     Route::get('user-by-dni/{dni}', [AdminController::class, 'userByDni']);
     Route::post('users/{user}/sync-permissions', [AdminController::class, 'syncPermissions']);
 });
@@ -33,3 +35,8 @@ Route::middleware('auth:sanctum')->prefix('admin')->group(function () {
 /* Ruta para probar si se genera el pdf */
 Route::get('prueba_pdf', [PDFController::class, 'pdf']);
 // });
+
+Route::get('datos_acta', [DatosActaController::class, 'index']);
+Route::get('datos_acta/{oficina}', [DatosActaController::class, 'getDatosPorOficina']);
+Route::get('registar_acta', [ActaController::class, 'store']);
+Route::post('agrupar_actas', [ActaController::class, 'agrupar']);
