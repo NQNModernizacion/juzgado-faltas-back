0c55635b37d52a023b512406400fc76a710a5d8d
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2026-05-07 12:41:59 -0300

    Update Padron.php

--- app/Models/Padron.php
+++ app/Models/Padron.php
@@ -9,5 +9,17 @@ class Padron extends Model
 {
     //
     use SoftDeletes;
+    protected $fillable = [
+        'tipo_id',
+        'identificacion',
+        'nombre'
+    ];
     protected $table = 'padrones';
+
+    public function actas()
+    {
+        return $this->belongsToMany(Acta::class, 'acta_padron')
+            ->withPivot('categoria_padron_id')
+            ->withTimestamps();
+    }
 }
