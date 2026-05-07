6dade61c99b1dda50df27af3e6c13fc16cc7ceaf
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2026-05-07 12:42:19 -0300

    Create Juez.php

---
+++ app/Models/Juez.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Juez extends Model
+{
+    //
+    use SoftDeletes;
+
+    protected $table = 'juez';
+
+    protected $fillable = [];
+
+    protected $hidden = [];
+}
