82986ea1d3026bdc4f6f49b06e81e902be883877
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2026-05-07 12:42:32 -0300

    Create UpdateJuezRequest.php

---
+++ app/Http/Requests/UpdateJuezRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UpdateJuezRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    protected function failedValidation(Validator $validator): void
+    {
+        throw new HttpResponseException(
+            sendResponse(null, $validator->errors(), 422)
+        );
+    }
+
+    protected function failedAuthorization(): void
+    {
+        throw new HttpResponseException(
+            sendResponse(null, 'No autorizado', 403)
+        );
+    }
+}
