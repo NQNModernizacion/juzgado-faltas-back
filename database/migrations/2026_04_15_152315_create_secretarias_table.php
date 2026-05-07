a342607daaa386dc1c4b350bfb6f5ca5d3af1dd4
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2026-05-07 12:41:52 -0300

    Create 2026_04_15_152315_create_secretarias_table.php

---
+++ database/migrations/2026_04_15_152315_create_secretarias_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('secretarias', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('codigo')->nullable();
+            $table->string('descripcion')->nullable();
+            $table->string('secretaria')->nullable();
+            $table->dateTime('fecha_desde')->nullable();
+            $table->dateTime('fecha_hasta')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('secretarias');
+    }
+};
