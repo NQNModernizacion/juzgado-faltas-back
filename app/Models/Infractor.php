7b3a3670b104baf5abfaa2dea609649a728c6062
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2026-05-07 12:42:16 -0300

    Update Infractor.php

--- app/Models/Infractor.php
+++ app/Models/Infractor.php
@@ -9,4 +9,19 @@ class Infractor extends Model
 {
     use SoftDeletes;
     protected $table = 'infractores';
+
+    protected $fillable = [
+        'tipo_id',
+        'documento',
+        'identificacion',
+        'nombre',
+        'domicilio'
+    ];
+
+    public function actas()
+    {
+        return $this->belongsToMany(Acta::class, 'acta_infractores')
+            ->withPivot('categoria_infractor_id', 'observaciones')
+            ->withTimestamps();
+    }
 }
