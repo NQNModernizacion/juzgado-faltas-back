4019b1f2905e294805477e37392c4ceb55ffae90
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2026-06-05 14:29:40 -0300

    Create 2026_05_19_135403_add_fecha_infraccion_and_lugar_to_acta_infraccion_table.php

---
+++ database/migrations/2026_05_19_135403_add_fecha_infraccion_and_lugar_to_acta_infraccion_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('acta_infraccion', function (Blueprint $table) {
+            $table->dateTime('fecha_infraccion')->nullable();
+            $table->string('lugar')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('acta_infraccion', function (Blueprint $table) {
+            $table->dropColumn(['fecha_infraccion', 'lugar']);
+        });
+    }
+};
