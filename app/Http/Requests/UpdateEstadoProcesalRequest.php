c5ea997105abe314e51ca2e9b38d0bd64ffb9c14
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2026-07-03 10:31:12 -0300

    Create UpdateEstadoProcesalRequest.php

---
+++ app/Http/Requests/UpdateEstadoProcesalRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Traits\TraitRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UpdateEstadoProcesalRequest extends FormRequest
+{
+    use TraitRequest;
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        $id = $this->route('estados_procesale');
+
+        return [
+            'estado' => 'required|string|max:10|unique:estados_procesales,estado,' . $id,
+            'descripcion' => 'required|string|max:255',
+            'tipo' => 'nullable|integer',
+            'es_antec' => 'boolean',
+            'antec_vig_dias' => 'nullable|integer',
+            'porc_bonif' => 'nullable|integer',
+            'bonif_vig_dias' => 'nullable|integer',
+            'gen_notif' => 'boolean',
+            'notif_cant_dias' => 'nullable|integer',
+            'form_autom' => 'nullable|boolean',
+            'perm_pago' => 'boolean',
+            'perm_plan' => 'boolean',
+            'perm_vol' => 'boolean',
+            'desestima' => 'boolean',
+            'tipo_causa' => 'nullable|string|max:255',
+        ];
+    }
+}
