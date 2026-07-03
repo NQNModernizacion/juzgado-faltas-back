ef5e5d7969088f105200f7ca69d0f5f0b8928039
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2026-07-03 10:30:17 -0300

    Create EstadoProcesalResource.php

---
+++ app/Http/Resources/EstadoProcesalResource.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EstadoProcesalResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $data = [
+            'id' => $this->id,
+            'estado' => $this->estado,
+            'descripcion' => $this->descripcion,
+            // 'tipo' => $this->tipo,
+            // 'es_antec' => (bool)$this->es_antec,
+            // 'antec_vig_dias' => $this->antec_vig_dias,
+            // 'porc_bonif' => $this->porc_bonif,
+            // 'bonif_vig_dias' => $this->bonif_vig_dias,
+            // 'gen_notif' => (bool)$this->gen_notif,
+            // 'notif_cant_dias' => $this->notif_cant_dias,
+            // 'form_autom' => (bool)$this->form_autom,
+            // 'perm_pago' => (bool)$this->perm_pago,
+            // 'perm_plan' => (bool)$this->perm_plan,
+            // 'perm_vol' => (bool)$this->perm_vol,
+            // 'desestima' => (bool)$this->desestima,
+            // 'tipo_causa' => $this->tipo_causa,
+        ];
+
+        // Incluir datos de la tabla pivot si existe relación
+        if ($this->pivot) {
+            $data['datos_adicional'] = [
+                'id' => $this->pivot->id,
+                'fecha' => $this->pivot->fecha,
+                'observacion' => $this->pivot->observacion,
+                'infractor_id' => $this->pivot->infractor_id,
+                'imputado_datos' => $this->pivot->imputado_datos,
+                'user_id' => $this->pivot->user_id,
+            ];
+        }
+
+        return $data;
+    }
+}
