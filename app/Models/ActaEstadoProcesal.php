4454bffabdebd77a3d529f2ce42cc6d06877fde0
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2026-07-03 10:30:04 -0300

    Create ActaEstadoProcesal.php

---
+++ app/Models/ActaEstadoProcesal.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class ActaEstadoProcesal extends Pivot
+{
+    protected $table = 'acta_estado_procesal';
+
+    public $incrementing = true;
+
+    protected $fillable = [
+        'acta_id',
+        'estado_procesal_id',
+        'fecha',
+        'observacion',
+        'infractor_id',
+        'imputado_datos',
+        'user_id',
+    ];
+
+    /**
+     * User who made the transition.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * Associated infractor (imputado), if any.
+     */
+    public function infractor()
+    {
+        return $this->belongsTo(Infractor::class, 'infractor_id');
+    }
+
+    /**
+     * Associated Acta.
+     */
+    public function acta()
+    {
+        return $this->belongsTo(Acta::class, 'acta_id');
+    }
+
+    /**
+     * Associated EstadoProcesal.
+     */
+    public function estadoProcesal()
+    {
+        return $this->belongsTo(EstadoProcesal::class, 'estado_procesal_id');
+    }
+}
