bfe05c8e09b6bfd48f942877cf017179781d5ed9
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2026-07-14 12:04:30 -0300

    Update EstadoProcesalResource.php

--- app/Http/Resources/EstadoProcesalResource.php
+++ app/Http/Resources/EstadoProcesalResource.php
@@ -18,6 +18,7 @@ class EstadoProcesalResource extends JsonResource
             'id' => $this->id,
             'estado' => $this->estado,
             'descripcion' => $this->descripcion,
+            'nombre' => $this->descripcion,
             // 'tipo' => $this->tipo,
             // 'es_antec' => (bool)$this->es_antec,
             // 'antec_vig_dias' => $this->antec_vig_dias,
@@ -35,14 +36,20 @@ class EstadoProcesalResource extends JsonResource
 
         // Incluir datos de la tabla pivot si existe relación
         if ($this->pivot) {
-            $data['datos_adicional'] = [
-                'id' => $this->pivot->id,
-                'fecha' => $this->pivot->fecha,
-                'observacion' => $this->pivot->observacion,
-                'infractor_id' => $this->pivot->infractor_id,
-                'imputado_datos' => $this->pivot->imputado_datos,
-                'user_id' => $this->pivot->user_id,
-            ];
+            $data['id'] = $this->pivot->id;
+            $data['fecha'] = $this->pivot->fecha;
+            $data['observacion'] = $this->pivot->observacion;
+            $data['infractor_id'] = $this->pivot->infractor_id;
+            $data['imputado_datos'] = $this->pivot->imputado_datos;
+            $data['user_id'] = $this->pivot->user_id;
+            // $data['datos_adicional'] = [
+            //     'id' => $this->pivot->id,
+            //     'fecha' => $this->pivot->fecha,
+            //     'observacion' => $this->pivot->observacion,
+            //     'infractor_id' => $this->pivot->infractor_id,
+            //     'imputado_datos' => $this->pivot->imputado_datos,
+            //     'user_id' => $this->pivot->user_id,
+            // ];
         }
 
         return $data;
