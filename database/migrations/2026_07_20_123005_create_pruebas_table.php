dd11e885cacc890bcd542d6850229d18fa64290d
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2026-07-20 14:53:30 -0300

    Create 2026_07_20_123005_create_pruebas_table.php

---
+++ database/migrations/2026_07_20_123005_create_pruebas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pruebas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('acta_id')->constrained('actas')->onDelete('cascade');
+            $table->string('tipo_archivo');
+            $table->text('observacion')->nullable();
+            $table->unsignedBigInteger('user_id'); // Sin foreign key física por estar en otra conexión (admin)
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pruebas');
+    }
+};
